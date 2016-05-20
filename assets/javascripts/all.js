2f1d06f7f9fa662bb20ee775ecfcda5f12bf13c1
mariafaulisi <maria@kohactive.com>
2016-05-20 12:11:16 -0500

    Site updated to 14d14c1

--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -13,6 +13,12 @@ $(document).ready(function(){
 });
 
 
+$(window).ready(function(){
+  $('.hamburger-wrapper').on('click', function(){
+    $(this).parent().find('.link-container').toggleClass('show-mobile-nav');
+  });
+});
+
 
 
 
