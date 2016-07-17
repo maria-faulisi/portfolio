295997f5a71f1c0c0460633bde1d35d1bfe51fb7
maria-faulisi <mjfaulisi@gmail.com>
2016-07-17 13:31:57 -0500

    Add info alert to floor

--- source/assets/javascripts/navigation.js
+++ source/assets/javascripts/navigation.js
@@ -5,6 +5,17 @@ $(window).ready(function(){
 
   $('#walk-in-trigger').on('click', function(e){
     e.preventDefault();
-    $(this).parent().next().slideToggle();
+    $this = $(this);
+    $parent = $this.parent();
+    $estWait = $('.estimated-wait');
+    $inputValue = $parent.find('input').val();
+    
+    if ($inputValue == '') {
+      $estWait.append().html('<h3>Estimated wait time for a new party of 3: <span>22 Minutes</span></h3>');  
+    }else{
+      $estWait.append().html('<h3>Estimated wait time for a new party of ' + $inputValue + ' : <span>22 Minutes</span></h3>');
+    }
+    $parent.next().slideToggle();
   });
+
 });
