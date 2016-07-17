b990f19b8129bd43e36b32120f50cca2543ebe31
maria-faulisi <mjfaulisi@gmail.com>
2016-07-17 12:26:06 -0500

    Floor and Walk-in actions working * nav links working * modal redirect working * walk-in slideToggle

--- source/assets/javascripts/navigation.js
+++ source/assets/javascripts/navigation.js
@@ -2,4 +2,9 @@ $(window).ready(function(){
   $('.hamburger-wrapper').on('click', function(){
     $(this).parent().find('.link-container').toggleClass('show-mobile-nav');
   });
+
+  $('#walk-in-trigger').on('click', function(e){
+    e.preventDefault();
+    $(this).parent().next().slideToggle();
+  });
 });
