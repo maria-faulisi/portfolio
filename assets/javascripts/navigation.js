063dc35adc1d1188bcee93738050f5a3795bd910
Maria Faulisi <mariafaulisi@Marias-MacBook-Pro.local>
2017-02-10 22:32:53 -0600

    Site updated to e2165e7

--- assets/javascripts/navigation.js
+++ assets/javascripts/navigation.js
@@ -2,20 +2,4 @@ $(window).ready(function(){
   $('.hamburger-wrapper').on('click', function(){
     $(this).parent().find('.link-container').toggleClass('show-mobile-nav');
   });
-
-  $('#walk-in-trigger').on('click', function(e){
-    e.preventDefault();
-    $this = $(this);
-    $parent = $this.parent();
-    $estWait = $('.estimated-wait');
-    $inputValue = $parent.find('input').val();
-    
-    if ($inputValue == '') {
-      $estWait.append().html('<h3>Estimated wait time for a new party of 3: <span>22 Minutes</span></h3>');  
-    }else{
-      $estWait.append().html('<h3>Estimated wait time for a new party of ' + $inputValue + ' : <span>22 Minutes</span></h3>');
-    }
-    $parent.next().slideToggle();
-  });
-
 });
