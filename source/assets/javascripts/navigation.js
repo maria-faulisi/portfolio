b105ce24ed262b387014b76182006a50fbe6125d
Maria Faulisi <mariafaulisi@Marias-MacBook-Pro.local>
2017-03-25 12:01:56 -0500

    Updates to footer styling

--- source/assets/javascripts/navigation.js
+++ source/assets/javascripts/navigation.js
@@ -1,5 +1,6 @@
 $(window).ready(function(){
-  $('.mobile-nav-trigger').click(function(){
+  $('.mobile-nav-trigger').click(function(e){
+    e.preventDefault();
     $('.navigation').toggleClass('mobile-show');
   });
 });
