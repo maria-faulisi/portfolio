9a71a3ea0eaebaff1b2eb830106c2d5a21591917
Maria Faulisi <mariafaulisi@Marias-MacBook-Pro.local>
2017-03-25 21:14:21 -0500

    Site updated to 66efa87

--- assets/javascripts/modal.js
+++
@@ -1,8 +0,0 @@
-$(window).ready(function(){
-  $('#exampleModal').on('show.bs.modal', function (event) {    
-    var triggerDiv = $(event.relatedTarget); 
-    var dataSrc = triggerDiv.data('image-src');
-    var modal = $(this);
-    modal.find('.modal-body img').attr('src', "assets/images/wf-full/" + dataSrc);
-  });
-});
